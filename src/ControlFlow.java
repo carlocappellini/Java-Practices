b0f90b14fedcc84373a68b963716ad860e216ab6
Bolimbo <carlo.cappellini@yahoo.com>
2018-05-23 22:07:53 -0500

    do while loop

---
+++ src/ControlFlow.java
@@ -0,0 +1,23 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+//        int i = 5;
+
+//        System.out.println(i);
+//        while( i < 15){
+//            i  = i+1;
+//
+//            System.out.println(i);
+//
+//
+//        }
+        int j = 0;
+        do{
+
+
+            System.out.println(j = j+2);
+
+        } while (j < 100);
+
+
+    }
+}
