778a9cd11c62cd5392d5187cd9b736d7a4273170
Bolimbo <carlo.cappellini@yahoo.com>
2018-05-23 23:16:11 -0500

    for loops program squared and cubed

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlow {
     public static void main(String[] args) {
 //        int i = 5;
@@ -39,24 +41,51 @@ public class ControlFlow {
 //        }while (i > 0);
 
 
-        for (int i = 0; i <= 100; i++) {
+//        for (int i = 0; i <= 100; i++) {
+//
+//
+//
+//            if(i % 5 == 0 && i % 3 == 0){
+//                System.out.println("fizzBuzz" + " " + i);
+//            }
+//            if (i % 5 == 0) {
+//                System.out.println("Buzz" + " " + i);
+//            }
+//
+//            if (i % 3 == 0 ){
+//                System.out.println("fizz" + " " + i);
+//
+//            }
+//
+//
+//        }
+
+
+        System.out.println("what number would you like to go up to");
+
+
+        Scanner sc = new Scanner(System.in);
+        int input;
+
+
+        input = sc.nextInt();
+        System.out.println(input);
+
+        System.out.println("number | squared | cubed");
+
+        System.out.println("-------|---------|------|");
+       for(int i = 0; i <= input; i++){
+
+           System.out.println(i + "      |   " + (
+                   (i * i) + "     | " +
+                   (i * i * i)));
+
+       }
 
 
 
-            if(i % 5 == 0 && i % 3 == 0){
-                System.out.println("fizzBuzz" + " " + i);
-            }
-            if (i % 5 == 0) {
-                System.out.println("Buzz" + " " + i);
-            }
-
-            if (i % 3 == 0 ){
-                System.out.println("fizz" + " " + i);
-
-            }
 
 
-        }
 
     }
 }
